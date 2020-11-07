31ec629282b9a8ea907f816fb378c66226837e84
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-11-07 20:26:06 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
+const obj = JSON.stringify({
+  phone: "18602100000",
+  email: "guestcaredominos@jublfood.com"
+});
 
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.statusCode = 200;
+    res.write("Welcome to Dominos!");
+    res.end();
+  }
+  if (req.url === "/contact") {
+    res.statusCode = 200;
+    res.write(obj);
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.statusMessage = "Not found";
+    res.end();
+  }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
