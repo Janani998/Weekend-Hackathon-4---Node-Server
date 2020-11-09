1dbe49f3440db61824645d0d92c7415ae53b374f
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-11-09 18:23:17 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const obj = JSON.stringify({
 
 function handleServer(req, res) {
   if (req.url === "/welcome") {
-    res.statusCode = 200;
+    res.status = 200;
     res.write("Welcome to Dominos!");
     res.end();
   }
   if (req.url === "/contact") {
-    res.statusCode = 200;
+    res.status = 200;
     res.write(obj);
     res.end();
   } else {
-    res.statusCode = 404;
+    res.status = 404;
     res.statusMessage = "Not found";
     res.end();
   }
